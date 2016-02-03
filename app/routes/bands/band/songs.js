d1c1ec1dfd73704c021afe1d5a80d6560b79b416
Justin Steenhuis <justinsteenhuis@gmail.com>
2016-02-03 10:04:18 +0100

    Pagina 70

---
+++ app/routes/bands/band/songs.js
@@ -0,0 +1,6 @@
+// app/routes/bands/band/songs.js
+import Ember from 'ember';
+export default Ember.Route.extend({ model: function() {
+return this.modelFor('bands.band');
+}
+});
---
+++ tests/unit/routes/bands/band---path=':slug'-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:bands/band---path=':slug'', 'Unit | Route | bands/band   path=':slug'', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
